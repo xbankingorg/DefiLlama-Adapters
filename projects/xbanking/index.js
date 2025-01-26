8462b7939e013f670bd76f2a2e439f428520f624
XBANKING <126655346+xbankingorg@users.noreply.github.com>
2025-01-27 00:15:37 +0700

    Revert "test"

--- projects/xbanking/index.js
+++ projects/xbanking/index.js
@@ -1,12 +1,23 @@
-async function tvl() {
-  return {
-    solana: 1e18,
-  };
+const sdk = require('@defillama/sdk');
+const ADDRESSES = require('../helper/coreAssets.json')
+const ETHEREUM_CONTRACT = "0xC4356aF40cc379b15925Fc8C21e52c00F474e8e9";
+
+
+async function tvl(_, _1, _2, { api }) {
+  const bal = await api.call({
+    abi: 'function getTvl() external view returns (uint256)',
+    target: ETHEREUM_CONTRACT,
+  });
+  api.add(ADDRESSES.null, bal)
 }
+
+
 
 module.exports = {
   timetravel: false,
-  misrepresentedTokens: true,
-  methodology: "test tvl xbanking",
-  solana: { tvl },
+  misrepresentedTokens: false,
+  methodology: 'h1test',
+  ethereum: {
+    tvl
+  }
 };

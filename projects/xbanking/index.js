6d81437ca4c39e74ad71b0658e93bd26386c01d1
XBANKING <126655346+xbankingorg@users.noreply.github.com>
2025-01-27 00:33:43 +0700

    Update index.js

--- projects/xbanking/index.js
+++ projects/xbanking/index.js
@@ -1,12 +1,15 @@
+const axios = require('axios');
+
 async function tvl() {
+  const tvl = await axios.get("https://api.xbanking.org/v2/platform/tvl");
   return {
-    solana: 1e18,
+    solana: parseFloat(tvl.data)
   };
 }
 
 module.exports = {
   timetravel: false,
   misrepresentedTokens: true,
-  methodology: "test tvl xbanking",
+  methodology: "real tvl xbanking",
   solana: { tvl },
 };
